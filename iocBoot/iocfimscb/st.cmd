19c81607972e8a586271022f7e98ce78c7d32f18
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-07-23 14:55:48 +0200

    tweak to make silent for the simulator, and set proto to send cmd without an extra space

--- iocBoot/iocfimscb/st.cmd
+++ iocBoot/iocfimscb/st.cmd
@@ -20,8 +20,10 @@ epicsEnvSet("PORT", "FIMSCB")
 drvAsynIPPortConfigure($(PORT), "127.0.0.1:9999", 0, 0, 0)
 # <0x0d> \r
 # <0x0a> \n
-asynOctetSetInputEos($(PORT), 0, "\n")
-asynOctetSetOutputEos($(PORT), 0, "\r\n")
+asynOctetSetInputEos($(PORT), 0, "\r\n")
+asynOctetSetOutputEos($(PORT), 0, "\r")
+#asynOctetSetInputEos($(PORT), 0, "\n")
+#asynOctetSetOutputEos($(PORT), 0, "\r\n")
 
 dbLoadRecords("db/iocAdminSoft.db",  "IOC=${IOCST}")
 dbLoadRecords("db/fimscb.db",     "P=$(P)-$(R):FimSCB:,PORT=FIMSCB")
